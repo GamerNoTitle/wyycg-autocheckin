24c83b189dcf5a9d195c6a8afa7d6cdfa48eee7e
GamerNoTitle <bili33@87ouo.top>
2020-11-21 19:59:34 +0800

    签到post构造完成(未投入使用)

--- main.py
+++ main.py
@@ -2,11 +2,11 @@ import sys
 import requests as r
 import json
 
-sign=''
+sign='https://n.cg.163.com/api/v2/sign-today'
 current='https://n.cg.163.com/api/v2/client-settings/@current'
 cookie=sys.argv[1]
 
-header={
+getheader={
     'Host': 'n.cg.163.com',
     'Connection': 'keep-alive',
     'Accept': 'application/json, text/plain, */*',
@@ -22,15 +22,36 @@ header={
     'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja-JP;q=0.6,ja;q=0.5'  
 }
 
-def post(url,data,header):
+signheader={
+    'Accept': 'application/json, text/plain, */*',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'zh-CN,zh;q=0.9,en-US;q=0.8,en;q=0.7,ja-JP;q=0.6,ja;q=0.5',
+    'Authorization': cookie,
+    'Connection': 'keep-alive',
+    'Content-Length': '0',
+    'Host': 'n.cg.163.com',
+    'Origin': 'https://cg.163.com',
+    'Referer': 'https://cg.163.com/',
+    'Sec-Fetch-Dest': 'empty',
+    'Sec-Fetch-Mode': 'cors',
+    'Sec-Fetch-Site': 'same-site',
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36',
+    'X-Platform': '0'
+}
+
+def post(url,header):
     result=r.post(url=url,headers=header)
     return result
 
-def getme(url,data,header):
+def getme(url,header):
     result=r.get(url=url,headers=header)
     print(result)
     return result
 
 if __name__ == "__main__":
-    me=getme(current,None,header)
-    print(me.text)
+    me=getme(current,getheader)
+    print(me.text,end='\n')
+    signin=post(sign,signheader)
+    print(signin.text,end='\n')
+    print(signin.status_code)
+    
